2072c9845922e45a1b7b6b6eb21bd502c77d2818
Keverton Reis <kevertondrv@gmail.com>
2025-07-15 13:47:45 -0300

    install tsx package

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
 import fastify from "fastify";
 
+// GET, POST, PUT, PATCH e DELETE  
+
 const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello caraio'
+})
+
+//PARA RODAR O SERVER EM JS SEM CONVERTER/CRIAR ARQUIVO, EXECUTAR O COMANDO "NPX TSX ARQUIVO.EXTENSAO" -- RECOMENDADO PARA USAR EM DESENVOLVIMENTO, NAO RECOMENDADO PARA PRODUÇÃO
+
+app.listen ({
+  port: 3333
+}).then (() => {
+  console.log('HTTP SERVER RUNNING AT PORT 3333')
+})

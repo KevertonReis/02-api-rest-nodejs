68232406929bb6b62407c98b25582b3d6de82a35
Keverton Reis <kevertondrv@gmail.com>
2025-07-17 15:00:35 -0300

    configuration EsLint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import fastify from "fastify";
 
-// GET, POST, PUT, PATCH e DELETE  
+// GET, POST, PUT, PATCH e DELETE
 
-const app = fastify()
+const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello caraio'
-})
+app.get("/hello", () => {
+  return "Hello caraio";
+});
 
-//PARA RODAR O SERVER EM JS SEM CONVERTER/CRIAR ARQUIVO, EXECUTAR O COMANDO "NPX TSX ARQUIVO.EXTENSAO" -- RECOMENDADO PARA USAR EM DESENVOLVIMENTO, NAO RECOMENDADO PARA PRODUÇÃO
+// PARA RODAR O SERVER EM JS SEM CONVERTER/CRIAR ARQUIVO, EXECUTAR O COMANDO "NPX TSX ARQUIVO.EXTENSAO" -- RECOMENDADO PARA USAR EM DESENVOLVIMENTO, NAO RECOMENDADO PARA PRODUÇÃO
 
-app.listen ({
-  port: 3333
-}).then (() => {
-  console.log('HTTP SERVER RUNNING AT PORT 3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP SERVER RUNNING AT PORT 3333");
+  });
+
+// EcmaScript Lint = ES Lint

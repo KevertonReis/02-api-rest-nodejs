3a4a1e97702cc3d8033ec5f5e5780144645be9de
Keverton Reis <kevertondrv@gmail.com>
2025-07-23 15:29:06 -0300

    add migrations and att app.db

--- db/migrations/20250723142151_create-transactions.ts
+++ db/migrations/20250723142151_create-transactions.ts
@@ -1,10 +1,14 @@
 import type { Knex } from 'knex';
 
+// a partir do momento em que a migration é criada, ela nao pode ser editada
+
 // subir alterações na tabela
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('transactions', (table) => {
     table.uuid('id').primary();
     table.text('title').notNullable();
+    table.decimal('amount').notNullable();
+    table.timestamp('created at').defaultTo(knex.fn.now()).notNullable();
   });
 }
 

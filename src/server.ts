0a24fbb74b7ddce8d1dde2146ba6934249a0d106
Keverton Reis <kevertondrv@gmail.com>
2025-07-30 10:24:44 -0300

    finish database module

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,36 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import crypto from 'node:crypto';
+import { env } from './env';
 
 // GET, POST, PUT, PATCH e DELETE
-
 // Comando para limpar e padronizar o codigo de acordo com o ESLint-> npm run lint
+// PARA RODAR O SERVER EM JS SEM CONVERTER/CRIAR ARQUIVO, EXECUTAR O COMANDO "NPX TSX ARQUIVO.EXTENSAO" -- RECOMENDADO PARA USAR EM DESENVOLVIMENTO, NAO RECOMENDADO PARA PRODUÇÃO
+// EcmaScript Lint = ES Lint
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
 
-  return tables;
+      title: 'Transaction test',
+
+      amount: 1000,
+    })
+
+    .returning('*');
+
+  return transaction;
 });
 
-// PARA RODAR O SERVER EM JS SEM CONVERTER/CRIAR ARQUIVO, EXECUTAR O COMANDO "NPX TSX ARQUIVO.EXTENSAO" -- RECOMENDADO PARA USAR EM DESENVOLVIMENTO, NAO RECOMENDADO PARA PRODUÇÃO
-
 app
+
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
+
   .then(() => {
     console.log('HTTP SERVER RUNNING AT PORT 3333');
   });
-
-// EcmaScript Lint = ES Lint
